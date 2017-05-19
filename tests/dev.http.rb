946df01c0c1f95a25a7e660e45fd8f9ad83b1850
Miko O'Sullivan <miko@idocs.com>
2017-05-19 01:25:32 -0400

    Housecleaning.

--- tests/dev.http.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/ruby -w
-system 'clear' unless ENV['clear_done']
-require_relative './testmin.rb'
-puts TestMin::VERSION
--- tests/dev.messages.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/ruby -w
-system 'clear' unless ENV['clear_done']
-require_relative './testmin.rb'
-
-# submit results
-puts TestMin.message('submit-results', TestMin.settings['submit-site'])
